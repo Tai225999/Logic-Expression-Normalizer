1135af21517209543f1532e816122470299485d2
unknown <anh.pq220071@sis.hust.edu.vn>
2024-05-22 18:21:27 +0700

    feat: column and PI table

---
+++ Logic Expression Normalizer/src/column/PITable.java
@@ -0,0 +1,164 @@
+package column;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class PITable {
+    // private int numOfBits = 4;
+    public static void main(String[] args) {
+        Set<Integer> minterm = new HashSet<>(Set.of(4, 8, 9, 10, 11, 12, 14, 15));
+        // Set<Integer> minterm = new HashSet<>(Set.of(0, 1, 2, 5, 6, 7));
+        List<Set<Integer>> primeImplicants = new ArrayList<>();
+        /* 
+        primeImplicants.add(new HashSet<>(Set.of(0, 1)));
+        primeImplicants.add(new HashSet<>(Set.of(0, 2)));
+        primeImplicants.add(new HashSet<>(Set.of(1, 5)));
+        primeImplicants.add(new HashSet<>(Set.of(2, 6)));
+        primeImplicants.add(new HashSet<>(Set.of(5, 7)));
+        primeImplicants.add(new HashSet<>(Set.of(6, 7)));
+        */
+
+        primeImplicants.add(new HashSet<>(Set.of(4, 12)));
+        primeImplicants.add(new HashSet<>(Set.of(8, 9, 10, 11)));
+        primeImplicants.add(new HashSet<>(Set.of(8, 10, 12, 14)));
+        primeImplicants.add(new HashSet<>(Set.of(10, 11, 14, 15)));
+
+        Set<Integer> essentialIndices = findEssentialPrimeImplicants(primeImplicants, minterm);
+
+        List<Set<Integer>> essentialPrimeImplicants = new ArrayList<>();
+        List<Set<Integer>> finalImplicants = new ArrayList<>();
+
+        if (essentialIndices.isEmpty()) {
+            System.out.println("Currently no essential prime implicants found.");
+        } else {
+            System.out.println("Essential Prime Implicants:");
+            for (Integer index : essentialIndices) {
+                essentialPrimeImplicants.add(primeImplicants.get(index));
+            }
+        }
+
+        for (Set<Integer> essentialImplicant : essentialPrimeImplicants){
+            for (int minElement : essentialImplicant){
+                System.out.printf("%d ", minElement);
+            }
+            System.out.println("");
+        }
+
+        Set<Integer> finalIndices = findMinimumPrimeImplicants(primeImplicants, minterm, essentialIndices);
+
+        if (essentialIndices.isEmpty()) {
+            System.out.println("Cannot find prime implicants!!");
+        } else {
+            System.out.println("Selected Implicants:");
+            for (Integer index : finalIndices) {
+                finalImplicants.add(primeImplicants.get(index));
+            }
+        }
+
+        for (Set<Integer> finalImplicant : finalImplicants){
+            for (int minElement : finalImplicant){
+                System.out.printf("%d ", minElement);
+            }
+            System.out.println("");
+        }
+        
+    }    
+
+    private static Set<Integer> findEssentialPrimeImplicants(List<Set<Integer>> primeImplicants, Set<Integer> minterms) {
+        Set<Integer> essentialPrimeImplicants = new HashSet<>();
+
+        boolean[][] table = new boolean[primeImplicants.size()][minterms.size()];
+
+        int i = 0;
+        int j = 0;
+
+        for (Set<Integer> implicant : primeImplicants) {
+            j = 0;
+            for (int minterm: minterms){
+                if (implicant.contains(minterm)) {
+                    table[i][j] = true;
+                }
+                j++;
+                System.out.println(j);
+            }
+            i++;
+        }
+
+        // Print the table
+        for (i = 0; i < primeImplicants.size(); i++){
+            for (j = 0; j < minterms.size(); j++){
+                System.out.printf("%d ", (table[i][j]) ? 1 : 0);
+            }
+            System.out.println("");
+        }
+        
+        for (j = 0; j < minterms.size(); j++) {
+            int count = 0;
+            int position = -1;
+            for (i = 0; i < primeImplicants.size(); i++) {
+                if (table[i][j]) {
+                    count++;
+                    position = i;
+                    if (count > 1) break;
+                }
+            }
+            if (count == 1) {
+                essentialPrimeImplicants.add(position); // Add index of essential implicant
+            }
+        }
+
+        return essentialPrimeImplicants;
+    }
+
+    public String convertToLiteral(String binaryRepresentation) {
+        char[] variables = {'A', 'B', 'C', 'D'}; // Assuming maximum 4 variables
+    
+        StringBuilder booleanLiteral = new StringBuilder();
+        for (int i = 0; i < binaryRepresentation.length(); i++) {
+            if (binaryRepresentation.charAt(i) == '-') {
+                continue;
+            } else if (binaryRepresentation.charAt(i) == '0') {
+                booleanLiteral.append(variables[i]).append("'");
+            } else if (binaryRepresentation.charAt(i) == '1') {
+                booleanLiteral.append(variables[i]);
+            }
+        }
+        
+        return booleanLiteral.toString();
+    }
+
+    private static Set<Integer> findAdditionalPrimeImplicants(List<Set<Integer>> primeImplicants, Set<Integer> minterms, Set<Integer> essentialPrimeImplicants) {
+        Set<Integer> additionalPrimeImplicants = new HashSet<>();
+    
+        // Identify the minterms not covered by essential prime implicants
+        Set<Integer> uncoveredMinterms = new HashSet<>(minterms);
+        for (Integer implicantIndex : essentialPrimeImplicants) {
+            Set<Integer> implicant = primeImplicants.get(implicantIndex);
+            uncoveredMinterms.removeAll(implicant);
+        }   
+    
+        // Find additional prime implicants to cover the uncovered minterms
+        for (Integer minterm : uncoveredMinterms) {
+            for (int i = 0; i < primeImplicants.size(); i++) {
+                if (primeImplicants.get(i).contains(minterm)) {
+                    System.out.print(i);
+                    additionalPrimeImplicants.add(i);
+                    break;
+                }
+            }
+            System.out.println("");
+        }
+    
+        return additionalPrimeImplicants;
+    }
+    
+    // Combine essential and additional prime implicants
+    private static Set<Integer> findMinimumPrimeImplicants(List<Set<Integer>> primeImplicants, Set<Integer> minterms, Set<Integer> essentialPrimeImplicants) {
+        Set<Integer> additionalPrimeImplicants = findAdditionalPrimeImplicants(primeImplicants, minterms, essentialPrimeImplicants);
+        Set<Integer> minimumPrimeImplicants = new HashSet<>(essentialPrimeImplicants);
+        minimumPrimeImplicants.addAll(additionalPrimeImplicants);
+        return minimumPrimeImplicants;
+    }
+}
